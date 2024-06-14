7adc57ef8b757eee457b79cdfeb15724dd7e8d6f
SomyaGarg17 <128392359+SomyaGarg17@users.noreply.github.com>
2024-06-14 18:18:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -623,28 +623,27 @@ elif option=="Topic Modelling and Labelling":
 	    upload_csv = st.file_uploader("Upload your CSV file", type=['csv'])
 	    if upload_csv is not None:
 		    if st.button("Analyze CSV File"):
-			    col1, col2 = st.columns([1,2])
+			    col1, col2 = st.columns([1, 2])
 			    with col1:
 				    st.info("CSV File uploaded")
-				    csv_file = upload_csv.name
-				    with open(os.path.join(csv_file),"wb") as f: 
-					    f.write(upload_csv.getbuffer())
-				    print(csv_file)
-				    df = pd.read_csv(csv_file)
+				    df = pd.read_csv(upload_csv)
 				    df.columns = df.columns.str.strip()
 				    df = df.fillna('')
 				    df = df.astype(str)
 				    st.dataframe(df)
 			    with col2:
-				    data_list = df['Data'].tolist()
-				    industry_list = []
-				    for i in data_list:
-                        #Performs Topic Labelling
-					    industry = label_topic(i)
-					    industry_list.append(industry)
-				    df['Industry'] = industry_list
-				    st.info("Topic Modeling and Labeling")
-				    st.dataframe(df)
+				    if 'Data' in df.columns:
+					    data_list = df['Data'].tolist()
+					    industry_list = []
+					    for i in data_list:
+						    industry = label_topic(i)
+						    industry_list.append(industry)
+					    df['Industry'] = industry_list
+					    st.info("Topic Modeling and Labeling")
+					    st.dataframe(df)
+				    else:
+					    st.error("The column 'Data' does not exist in the uploaded CSV file.")
+					    
 #Email Spam Classifier                    
 elif option == "Email Spam Classifier":
     st.title("Email Spam Classifier")
